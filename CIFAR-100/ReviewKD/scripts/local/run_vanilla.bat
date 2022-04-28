9f1345de1600f2b0397d83342394ac68322e83b4
unknown <dingx92@gmail.com>
2022-04-28 22:54:30 +0800

    delete files

--- CIFAR-100/ReviewKD/scripts/local/run_vanilla.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set ROOT_PATH="G:/OneDrive/Working_directory/cGAN-KD/CIFAR-100/ReviewKD"
-set DATA_PATH="G:/OneDrive/Working_directory/datasets/CIFAR-100/data"
-
-python train.py ^
-    --root_path %ROOT_PATH% --data_path %DATA_PATH% ^
-    --model MobileNetV2 --mode vanilla ^ %*
-
